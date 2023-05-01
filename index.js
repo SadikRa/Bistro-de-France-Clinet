bb4c1cf64441b002cbee1812ebb15f7e06f2b07a
SadikRa <sadikrahman494@gmail.com>
2023-05-01 21:25:05 +0600

    first local server host done

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
-const port = 5000
-app.use(cors())
 
+const port = 5000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
