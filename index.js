3375f468008b1b4bd0095742f0ec6fa624ed4845
SadikRa <sadikrahman494@gmail.com>
2023-05-02 15:41:36 +0600

    json data added done

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = 5000
+app.use(cors())
 
+
+const Chef = require('./data/Chef.json')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/chef' , (req, res) =>{
+  res.send(Chef)
 })
 
 app.listen(port, () => {
