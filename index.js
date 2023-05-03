cecd0602a4a41e73bf83a294e93c0ec359d26828
SadikRa <sadikrahman494@gmail.com>
2023-05-03 21:18:38 +0600

    assessment done

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ app.get('/chef' , (req, res) =>{
   res.send(Chef)
 })
 
-
 app.get('/chef/:id', (req, res) => {
-  const id = req.params.id;
-  const ChefDetails = Chef.find(data => data.id === id);
+  const id = parseInt(req.params.id);
+  const ChefDetails = Chef.find(data => parseInt(data.id) === id);
   res.send(ChefDetails)
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
